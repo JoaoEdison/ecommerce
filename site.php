78df322394abeaa2d71844ef1ec96da929864c23
JoaoEdison <joaoedison342@outlook.com>
2019-07-01 01:30:03 -0300

    Formatação dos preços

--- site.php
+++ site.php
@@ -1,11 +1,16 @@
 <?php
 
 use \Hcode\page;
+use \Hcode\Model\Product;
 
 $app->get('/', function() {
     
+    $products = Product::listAll();
+    
     $page = new page();
     
-    $page->setTpl("index");
+    $page->setTpl("index", [
+        "products"=>Product::checkList($products)
+    ]);
     
 });
