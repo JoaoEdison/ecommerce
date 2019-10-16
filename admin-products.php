0b68b895b23fd48a1d24839bc3f197e36bc7d24d
JoaoEdison <joaoedison342@outlook.com>
2019-10-16 01:44:42 -0300

    Ferramenta de busca concluida

--- admin-products.php
+++ admin-products.php
@@ -8,12 +8,40 @@ $app->get('/admin/products', function(){
    
    User::verifyLogin(); 
    
-   $products = Product::listAll();
+    $search = (isset($_GET['search'])) ? $_GET['search'] : '';
+    $page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
+    
+    if ($search != '') {
+        
+        $pagination = Product::getPage($page, $search);
+        
+    } else {
+        
+        $pagination = Product::getPage($page);
+        
+    }
+    
+    $pages = [];
+    
+    for ($x = 0; $x < $pagination['pages']; $x++)
+    {
+        
+        array_push($pages, [
+           'href'=>'/admin/products?'. http_build_query([
+               'page'=>$x + 1,
+               'search'=>$search
+           ]),
+            'text'=>$x + 1
+        ]);
+        
+    }   
    
    $page = new pageadmin(); 
     
-   $page->setTpl("products", array(
-       "products"=>$products
+   $page->setTpl("products", array(        
+        "products"=>$pagination['data'],
+        "search"=>$search,
+        "pages"=>$pages           
    ));
    
 });
