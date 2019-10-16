1767175c6c87abb133313388c73a4c95e0f54467
JoaoEdison <joaoedison342@outlook.com>
2019-10-16 00:40:57 -0300

    Categorias busca

--- vendor/php-classes/src/Model/Category.php
+++ vendor/php-classes/src/Model/Category.php
@@ -157,4 +157,45 @@ class Category extends Model {
         ]);
         
     }
+    
+    public static function getPage($page = 1, $search = '', $itemsPerPage = 10) 
+    {
+    
+        $start = ($page - 1) * $itemsPerPage;
+        
+        $sql = new Sql();
+        
+        if($search != ''){
+            
+            $results = $sql->select("
+                SELECT SQL_CALC_FOUND_ROWS * 
+                FROM tb_categories
+                WHERE descategory LIKE :search                 
+                ORDER BY descategory
+                LIMIT $start, $itemsPerPage                              
+            ", [
+                ":search"=>'%'.$search.'%'
+            ]);
+            
+        } else { 
+            
+            $results = $sql->select("                   
+                SELECT SQL_CALC_FOUND_ROWS * 
+                FROM tb_categories
+                ORDER BY descategory
+                LIMIT $start, $itemsPerPage  
+            ");
+            
+        }
+        
+        
+        $resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+        
+        return [
+            'data'=>$results,
+            'total'=>(int)$resultTotal[0]["nrtotal"],
+            'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage)
+        ];
+        
+    }
 }
